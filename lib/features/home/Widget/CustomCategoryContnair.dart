420eff05dd83618cac2d62c7a5845f2baf72d0d2
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-31 16:16:18 +0100

    custom category,custom section,

--- lib/features/home/Widget/CustomCategoryContnair.dart
+++ lib/features/home/Widget/CustomCategoryContnair.dart
@@ -1,10 +1,32 @@
+import 'package:dev_challenge_2/core/Theme/AppColors.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomCategoryC extends StatelessWidget {
-  const CustomCategoryC({super.key});
+  final String category;
+  final Function() ? ontap;
+  const CustomCategoryC({super.key, required this.category, this.ontap});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return InkWell(
+      onTap: ontap,
+      child: Container(
+        height: 41.h,
+        width: 92.w,
+        color: AppColors.Category,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40.r).w,
+        ),
+        child: Text(
+          category,  
+          style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w400,
+              color: AppColors.textColor),
+        ),
+      ),
+    );
   }
 }
