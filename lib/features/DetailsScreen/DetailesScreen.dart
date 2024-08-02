8ca9f5183b63002b20f9645536cd67a9696752af
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-08-02 01:12:50 +0100

    first commit in details Screen

---
+++ lib/features/DetailsScreen/DetailesScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsState();
+}
+
+class _DetailsState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/features/Search Screen/SearchScreen.dart
+++ lib/features/Search Screen/SearchScreen.dart
@@ -1,10 +1,10 @@
 import 'package:dev_challenge_2/core/Theme/AppSizes.dart';
+import 'package:dev_challenge_2/core/Ui/Animation/TransitionAnimationRoute.dart';
 import 'package:dev_challenge_2/features/Search%20Screen/Widget/CustomGridChoice.dart';
 import 'package:dev_challenge_2/features/Search%20Screen/Widget/CustomSerachBar.dart';
 import 'package:dev_challenge_2/features/home/Widget/BuildCategory.dart';
 import 'package:dev_challenge_2/features/home/Widget/CustomNameSection.dart';
 import 'package:flutter/material.dart';
-
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -17,28 +17,37 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Search',style: TextStyle(fontWeight: FontWeight.bold),),
-        
-      ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomSearchBar(),
-            SizedBox(height: AppSizes.height,),
-            BuildCategory(),    
-             CustomNameSection( 
-            tit: 'View All', 
-            NSection: ' Recipes',
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Search',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: AppSizes.height,),
-          CustomGridChoice(descp: 'Easy homemade beef burger', name: 'James Spader'),
-          ]
-          
         ),
-      )
-      );
-    
+        body: SingleChildScrollView(
+          child: Column(children: [
+            const CustomSearchBar(),
+            const SizedBox(
+              height: AppSizes.height,
+            ),
+            const BuildCategory(),
+            const CustomNameSection(
+              tit: 'View All',
+              NSection: ' Recipes',
+            ),
+            const SizedBox(
+              height: AppSizes.height,
+            ),
+            CustomGridChoice(
+                descp: 'Easy homemade beef burger',
+                name: 'James Spader',
+                onpress: () {
+                  /*Navigator.pushReplacement(
+                context,
+                FadeSlidePageTransition(page:)
+              );*/
+                }),
+          ]),
+        ));
   }
 }
